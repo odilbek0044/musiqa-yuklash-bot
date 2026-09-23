f7c461bdb66c6bf2e6c9cb1fc2928dc0ac052452
odilbek0044 <aktamovodil84@gmail.com>
2026-09-23 21:07:35 +0400

    bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def home():
 
 def run_web():
     port = int(os.environ.get("PORT", 8080))
-    flask_app.run(host="0.0.0.0", port=port)
+    flask_app.run(host="0.0.0.0", port=port, debug=False)
 
 threading.Thread(target=run_web, daemon=True).start()
 # ---------------------------------
